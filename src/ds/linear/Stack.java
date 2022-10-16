bd8f8f3abd40348655d6c856c3bd23f246311a49
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-16 16:02:21 +0530

    Added: UC2 Peek and pop from stack

--- src/ds/linear/Stack.java
+++ src/ds/linear/Stack.java
@@ -13,6 +13,18 @@ public class Stack<T> {
         list.add(item);
     }
 
+    T peek() {
+        return list.getLast();
+    }
+
+    T pop() {
+        return list.removeLast();
+    }
+
+    int size() {
+        return list.size();
+    }
+
     void show() {
         System.out.println(list);
     }
@@ -23,6 +35,16 @@ public class Stack<T> {
         nums.push(30);
         nums.push(56);
 
+        System.out.println("Elements in stack:");
+        nums.show();
+
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(nums.peek());
+            nums.pop();
+        }
+
+        System.out.println("Elements in stack after popping:");
         nums.show();
     }
 }
